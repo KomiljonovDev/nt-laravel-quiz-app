f04ffc821c99c3f5b2b69cc6f1d96aaea6f12956
Komiljonov O <uzokdeveloper@gmail.com>
2025-02-07 19:47:23 +0500

    MINOR: Implemented Breeze and Add Dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,27 @@
 <?php
 
-use App\Models\User;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use  App\Http\Controllers\QuizController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    $data = [
-        'user' => 'Abdulloh'
-    ];
-    return view('welcome',$data);
+Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+
+Route::get('/take-quiz', [QuizController::class, 'takeQuiz'])->middleware('auth')->name('take-quiz');
+Route::get('/dashboard', [DashboardController::class, 'home'])->name('home');
+
+
+
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('about', function () {
-    return view('about');
-});
-Route::get('take-quiz', function () {
-    return view('quiz.take-quiz');
-});
+
+require __DIR__.'/auth.php';
